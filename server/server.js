4e78f2f1ca288ffb274abb91f495e4130e3d0d31
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-15 12:09:15 -0800

    [feat] add basic express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var http = require('http');
 var app = express();
-
-var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var port = process.env.PORT;
 
 server.listen(port);
