b5696d3136f1175e1f4f81d64dbf26e53784d1f4
Anthony Bibbs <anthonybibbs@gmail.com>
2016-01-19 11:21:58 -0800

    [feature] Create authRoutes, authHandler and githubSessions for Github authorization

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,8 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var cors = require('cors');
+var path = require('path');
+var githubSessions = require('./githubSessions.js');
 
 module.exports = function(app, express) {
 
@@ -12,9 +14,22 @@ module.exports = function(app, express) {
   app.use(morgan('combined'));
   app.use(express.static(__dirname +  '/../../client'));
 
+  // initializes client sessions and Github login
+  githubSessions.initialize(app);
+
+  var authRouter = express.Router();
+
+  app.use('/auth', authRouter);
+
+  app.get('/failure', function(req, res) {
+    res.status('200');
+    res.send("you don't have access to that resource. redirecting to sign in.");
+  });
+
   // app.get('/', function(req, res) {
   //   res.status('200');
   //   res.send("Hello World");
   // });
+  require(__dirname + './../auth/authRoutes.js')(authRouter);
 };
 
