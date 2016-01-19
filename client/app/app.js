dc587d9cd4ced28b2fa480697cb0b4bf26e801f7
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-19 15:36:32 -0800

    [feat] add ng-cookies

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 angular.module('app', [
   'ui.router',
-  'app.home'
+  'app.home',
+  'ngCookies'
   ])
   .config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
