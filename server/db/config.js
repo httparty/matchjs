d31ab4df4eb76101525c6297ee01bde1c1dd3c44
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-18 17:28:29 -0800

    [feat] updated DB, comments, validate statements, and datatype changes, config.js

--- server/db/config.js
+++ server/db/config.js
@@ -12,7 +12,7 @@ var UserSkillJoin = require('./models/userSkills.js');
 
 // var sequelize = new Sequelize(process.env.DATABASE_URL); //Uncomment for real deployment
 
-var sequelize = new Sequelize('postgres://psoshnin:RIPmatchr28@postgres-match-dev.co6f9ijf2gka.us-west-2.rds.amazonaws.com:5432/matchjs'); //Comment out when actually deploying
+var sequelize = new Sequelize();
 
 sequelize
   .authenticate()
@@ -23,21 +23,109 @@ sequelize
   });
 
 var models = {};
+
 models.User = new User(sequelize, Sequelize);
 models.Skill = new Skill(sequelize, Sequelize);
 models.Message = new Message(sequelize, Sequelize);
 models.UserSkillJoin = new UserSkillJoin(sequelize, Sequelize);
 
+models.User = sequelize.define('User', {
+	username: {
+		type: Sequelize.STRING
+	},
+	name: {
+		type: Sequelize.STRING
+	},
+	password: {
+		type: Sequelize.STRING
+	},
+	email: {
+		type: Sequelize.STRING,
+		validate: {
+			isEmail: true
+		}
+	},
+	phoneNumber: {
+		type: Sequelize.STRING,
+		validate: {
+			len: [10,13]
+		}
+	},
+	photo: {
+		type: Sequelize.STRING
+	},
+	github: {
+		type: Sequelize.STRING
+	},
+	location: {
+		type: Sequelize.STRING
+	},
+	summary: {
+		type: Sequelize.TEXT
+	},
+	karmaPoints: {
+		type: Sequelize.INTEGER
+	}
+});
+
+models.Skill = sequelize.define('Skill', {
+	name: {
+		type: Sequelize.STRING
+	}
+});
+
+models.Message = sequelize.define('Message', {
+	senderName: {
+		type: Sequelize.STRING
+	},
+	recipientName: {
+		type: Sequelize.STRING
+	},
+	recipientID: {
+		type: Sequelize.INTEGER
+	},
+	text: {
+		type: Sequelize.TEXT
+	}
+});
+
+models.UserSkillJoin = sequelize.define('UserSkillJoin', {
+	toLearn: {
+		type: Sequelize.BOOLEAN
+	},
+	toTeach: {
+		type: Sequelize.BOOLEAN
+	}
+});
+
 models.User.hasMany(models.Message);
+//Because of this ^, Message model contains UserId
 models.Skill.belongsToMany(models.User, {through: 'UserSkillJoin'});
 models.User.belongsToMany(models.Skill, {through: 'UserSkillJoin'});
 
 models.User.sync({force: false}).then(function(){
 	console.log('User table created!');
+	// return models.User.create({
+ //    username: "Tom123",
+ //    name: "Tom Test",
+ //    password: "abc123",
+ //    email:"tom@tom.com"
+ //    // phone: 415-222-3215
+ //  });
+
 });
 
 models.Skill.sync({force: false}).then(function(){
 	console.log('Skill table created!');
+	// models.Skill.bulkCreate([
+	// {	name: "AngularJS" },
+	// { name: "JavaScript" },
+	// { name: "Express"}
+	// ]).then(function() {
+	// 	return models.Skill.findAll();
+	// }).then(function(skillsArr) {
+	// 	// console.log(skillsArr);
+	// })
 });
 
 models.Message.sync({force: false}).then(function(){
