6454bfa532784b04131e671c7514a5016a83d16b
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-19 17:05:28 -0800

    [feat] add basic client side auth in run function

--- client/app/app.js
+++ client/app/app.js
@@ -16,4 +16,25 @@ angular.module('app', [
       templateUrl: 'app/components/test/test.html',
       url: '/test'
     })
+  }])
+  .run(['$rootScope','$state','$cookies','$window',function($rootScope, $state, $cookies, $window) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) { 
+
+      //this value is undefined if a user has never logged in
+      //it's also undefined if a user logged out
+      var cookie = $cookies.get('connect.sid');
+      console.log("cookie", cookie);
+
+      if(!cookie && toState.name === "home") {
+        console.log("hey I'm logged out");
+        return;
+      } else if (!cookie && toState.name !== "home") {
+        console.log("I'm still logged out in another page");
+        event.preventDefault(); 
+        $state.go('home');
+        return;
+      } else {
+        console.log("hey I'm logged in");
+      }    
+    })
   }]);
