488435340e7130ce5d67d39890a02bb48a316197
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-20 11:54:58 -0800

    [feat] Integrate navbar and refactor

--- client/app/components/shared/navbar/navbarDirectives.js
+++ client/app/components/shared/navbar/navbarDirectives.js
@@ -1,7 +1,7 @@
 angular.module('app.navbarDirectives', [])
   .directive('navDir', function(){
     return {
-      templateUrl: 'app/partials/_navbar.html'
+      templateUrl: 'app/components/shared/navbar/_navbar.html'
       // controller: ['$scope', function($scope){
       //   $scope.Message = 'Hello World';
       // }]   //Still not working
