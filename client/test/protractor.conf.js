276d89eda14fdc0b124270e25714956bc1b83838
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-21 12:10:54 -0800

    Add multi-browser capability for Protractor By default, running `protractor client/test/protractor.conf.js` will just use Chrome. If one would like to run against both Firefox and Chrome, one needs to specify it like so: `protractor client/test/protractor.conf.js --prod`

--- client/test/protractor.conf.js
+++ client/test/protractor.conf.js
@@ -1,12 +1,31 @@
+var browsers = {
+  firefox: {
+    name: 'Firefox',
+    browserName: 'firefox'
+  },
+  chrome: {
+    name: 'Chrome',
+    browserName: 'chrome'
+  }
+}
+
 exports.config = {
-  
+
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:5000',
   framework: 'jasmine2',
   specs: [
     './e2e/*.spec.js'
-  ],
-  multiCapabilities: [{
-    browserName: 'chrome'
-  }]
+  ]
 };
+
+//Run just Chrome in Dev
+//In Travis, run against both browsers
+if (process.argv[3] === '--prod') {
+  exports.config.multiCapabilities = [
+    browsers.firefox,
+    browsers.chrome
+  ]
+} else {
+  exports.config.capabilities = browsers.chrome;
+}
