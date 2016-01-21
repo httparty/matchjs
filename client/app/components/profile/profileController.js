f43b6c9654b54acadf18010463710e9c7e23d670
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-21 10:26:03 -0800

    [feat] simple styling on profile, /profile

--- client/app/components/profile/profileController.js
+++ client/app/components/profile/profileController.js
@@ -13,7 +13,6 @@ angular.module('app.profile', [])
 		$scope.data.updateBasics = {}; //this is to pass new data into the form fields
 
 		$scope.user = angular.fromJson(AuthService.getCurrentUser());
-
 		// console.log("HERE IS USER", $scope.user);
 
 		$scope.saveEditButton = {};
@@ -21,43 +20,56 @@ angular.module('app.profile', [])
 		$scope.saveEditButton.skills.buttonText = 'Edit';
 		$scope.saveEditButton.basics = {};
 		$scope.saveEditButton.basics.buttonText = 'Edit';
+		$scope.saveEditButton.summary = {};
+		$scope.saveEditButton.summary.buttonText = 'Edit';
 
-		//called when BASICS edit/show button is clicked
+		$scope.selectedStyle = {};
+
+//-------------------BASICS----------------------------
 		$scope.toggleEditShowBasics = function() {
 			console.log('inside toggle edit show basics fn');
 			if($scope.saveEditButton.basics.buttonText === 'Edit') {
 				$scope.saveEditButton.basics.buttonText = 'Save';
+				$scope.selectedStyle.basics = {"background-color" : "#FFFFCC"};
 			} else {
 				$scope.saveEditButton.basics.buttonText='Edit';
+				$scope.selectedStyle.basics = {"background-color" : "#FFFFFF"};
 				$scope.updateProfileBasics(); //call to fn 
 			}
 		};
 
+
+//------------SHARED BY BASICS & SUMMARY---------------
 		$scope.updateProfileBasics = function() {
 			var userDataObj = $scope.data.updateBasics; 
 			console.log("here is userDataObj", userDataObj);
-			Profile.updateProfileBasics(userDataObj) //update DB
-				.then(function(response) {
-					console.log("here is the server response to BASICS update", response);
-					for(var key in userDataObj) { //update DOM 
-						if(userDataObj[key]) {
-							$scope.user[key] = userDataObj[key];
+			if(userDataObj) {
+				Profile.updateProfileBasics(userDataObj) //update DB
+					.then(function(response) {
+						console.log("here is the server response to BASICS update", response);
+						for(var key in userDataObj) { //update DOM 
+							if(userDataObj[key]) {
+								$scope.user[key] = userDataObj[key];
+							}
 						}
-					}
-				})
+					})
+			}
 		};
 
+//-------------------SKILLS----------------------------
 		//called when SKILLS edit/show button is clicked
 		$scope.toggleEditShowSkills = function() {
 			if($scope.saveEditButton.skills.buttonText === 'Edit') {
 				$scope.saveEditButton.skills.buttonText = 'Save';
+				$scope.selectedStyle.skills = {"background-color" : "#FFFFCC"};
 			} else {
 				$scope.saveEditButton.skills.buttonText='Edit';
+				$scope.selectedStyle.skills = {"background-color" : "#FFFFFF"};
 				$scope.updateProfileSkills(); //call to fn that saves the skills 
 			} 
 		};
 
-		//called from within toggleEditShow when save button is clicked 
+		//called from within toggleEditShowSkills
 		$scope.updateProfileSkills = function() {
 			console.log('hello inside update skills!');
 			var userDataObj = {};
@@ -70,19 +82,36 @@ angular.module('app.profile', [])
 				for(var toTeachKey in $scope.data.toTeach) {
 					userDataObj.toTeach.push(toTeachKey);
 				}
-			Profile.updateProfileSkills(userDataObj)
-				.then(function(response) {
-					console.log("here is the server response to SKILLS update", response);
-					//
-				})
+			if(userDataObj) {
+				Profile.updateProfileSkills(userDataObj)
+					.then(function(response) {
+						console.log("here is the server response to SKILLS update", response);
+						//
+					})
+			}
 		};
 
 
+//-------------------SUMMARY---------------------------
+		$scope.toggleEditShowSummary = function() {
+			if($scope.saveEditButton.summary.buttonText === 'Edit') {
+				$scope.saveEditButton.summary.buttonText = 'Save';
+				$scope.selectedStyle.summary = {"background-color" : "#FFFFCC"};
+			} else {
+				$scope.saveEditButton.summary.buttonText='Edit';
+				$scope.selectedStyle.summary = {"background-color" : "#FFFFFF"};
+				$scope.updateProfileBasics(); //call to fn that saves the skills 
+			} 
+		};
+
+
+//---------RETRIEVE USER PROFILE DATA FROM DB------------
 		//called on the initialization of the page
 		$scope.getCurrentUserProfile = function() {
 			console.log('hello inside get currentUserProfile');
 			var userDataObj = {};
 			userDataObj.username = $scope.user.username;
+			// $scope.data.updateBasics.summary = "hello here is test summary";
 			Profile.getCurrentUser(userDataObj)
 				.then(function(response) {
 					//for each toLearn skills array in response, 
@@ -91,8 +120,9 @@ angular.module('app.profile', [])
 					//for each toTeach skills array in response
 						//if skill value is true
 						//toggle checkbox to checked: $scope.data.toLearn[] = true;//SKILLNAME] 
+					//if response has summary
+						//$scope.data.updateBasics.summary = the summary from resp
 				})
-			//toggle true skills to true
 		};
 
   }]);
