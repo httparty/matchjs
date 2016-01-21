3fcb63866b3d371596f6491c43d420c7401c0f9e
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-20 19:09:24 -0800

    [refactor] update the routes, profileModel.js

--- client/app/components/profile/profileModel.js
+++ client/app/components/profile/profileModel.js
@@ -3,9 +3,8 @@ angular.module('app.profile')
 
   var getCurrentUser = function(userDataObj) {
     return $http({
-      method: 'POST',
-      url: '/api/users/user',
-      data: userDataObj
+      method: 'GET',
+      url: '/api/users/userProfile/' + userDataObj.username
     }).then(function(responseObj){
       return responseObj;
     });
@@ -14,7 +13,7 @@ angular.module('app.profile')
   var updateProfileSkills = function(userDataObj) {
     return $http({
       method: 'POST',
-      url: '/api/users/skills',
+      url: '/api/users/profileSkills',
       data: userDataObj
     }).then(function(responseObj){
       return responseObj;
@@ -24,7 +23,7 @@ angular.module('app.profile')
   var updateProfileBasics = function(userDataObj) {
     return $http({
       method: 'POST',
-      url: '/api/users/basics',
+      url: '/api/users/profileBasics',
       data: userDataObj
     }).then(function(responseObj){
       return responseObj;
