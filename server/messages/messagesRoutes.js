5622e02dc74334c3f5fc4f916138b97a98625704
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-21 14:27:18 -0800

    [refactor-inbox] Jshint fix

--- server/messages/messagesRoutes.js
+++ server/messages/messagesRoutes.js
@@ -2,5 +2,5 @@ var messagesHandler = require('./messagesHandler.js');
 
 module.exports = function(app) {
   app.post('/inbox/sendMessage', messagesHandler.sendMessage);
-  app.get('/inbox/displayMessages', messagesHandler.displayMessages)
+  app.get('/inbox/displayMessages', messagesHandler.displayMessages);
 };
