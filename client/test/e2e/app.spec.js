4cd362a2f031d7406ce13c331c69b43b05f591a5
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-21 11:14:57 -0800

    [refactor] create index page object for e2e

--- client/test/e2e/app.spec.js
+++ client/test/e2e/app.spec.js
@@ -1,12 +1,22 @@
+var IndexPage = require('./pages/index.page')
+
 describe("app", function () {
+
+  var page = new IndexPage();
+
+  beforeEach(function() {
+    page.get();
+  });
 
   describe("index", function () {
 
     it("should display the correct title", function () {
-      browser.get('/#');
-      expect(browser.getTitle()).toBe('MatchJS');
+      expect(page.getTitle()).toBe('MatchJS');
     });
 
+    it("should sign in when the log in button is clicked", function () {
+      // page.clickButton();
+    });
 
   });
 });
--- client/test/e2e/test.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
-});
