a95ed041355a43a9a2172215a042b43f1ad59081
Anthony Bibbs <anthonybibbs@gmail.com>
2016-01-20 16:28:07 -0800

    [fix] Update getAllUsers function to display to DOM

--- client/app/components/connect/connectController.js
+++ client/app/components/connect/connectController.js
@@ -1,10 +1,9 @@
 angular.module('app.connect',[])
 .controller('connectController', function($scope,connectModel) {
 	$scope.getAllUsers = function() {
-		var getUsers = connectModel.getAllUsers();
-		// console.log('HERE I AM: ',connectModel.getAllUsers());
-		// connectModel.getAllUsers();
-		// getUsers.value.data
-		console.log('HERE I AM: ',getUsers);
+		connectModel.getAllUsers().then(function(r) {
+			console.dir(r.data);
+			$scope.users = r.data;
+		});
 	};
 });
