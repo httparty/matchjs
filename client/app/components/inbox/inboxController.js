610c2579b3809eabadd7c560e7f4d267ee27a919
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-23 11:06:31 -0800

    [feat] Add basic messaging with Firebase

--- client/app/components/inbox/inboxController.js
+++ client/app/components/inbox/inboxController.js
@@ -1,97 +1,44 @@
 angular.module('app.inbox', ['firebase'])
   .controller('InboxController', ['$scope', 'Inbox', 'AuthService', 'connectModel', '$firebaseObject', '$firebaseArray',function($scope, Inbox, AuthService, connectModel, $firebaseObject, $firebaseArray){
 
-    // var myFirebaseRef = new Firebase("https://matchjs.firebaseio.com/chat");
-    var count = 0;
-    var baseURL = "https://matchjs.firebaseio.com/chat/";
-
-    console.log('Inbox Controller is working');
-
+    //Firebase setup
+    var baseURL = 'https://matchjs.firebaseio.com/chat/';
+    var myFirebaseRef = '';
 
     // Scope Variables
     var user = angular.fromJson(AuthService.getCurrentUser());
     $scope.username = user.username;
     $scope.conversationList = [];
 
-    connectModel.getAllUsers().then(function(r) {
-        console.dir(r.data);
-        $scope.conversationList = r.data;
-    });
-    // $scope.conversationList = ['Rachel', 'Polina', 'Anthony', 'Chris', 'Jeff'];
-    $scope.conversationObject = {
-        'Rachel': ['Hey!', 'How goes it?'],
-        'Polina': ['Hi! Do you need a mentor?'],
-        'Anthony': ['Whatsup man?', 'I was looking into this new framework, and was wondering if you wanted to help', 'It will be a lot of fun!'],
-        'Chris': ['You will go back to your desk and nope... you do not gots it', 'I am a programming god, I made fire', 'that is my goal here... crush your spirits'],
-        'Jeff': ['Im on vacation!']
-    };
-
     //Person with whom you are currently chatting
     $scope.currentRecipient = '';
     $scope.currentMessageList = [];
     $scope.enteredText = '';
 
-    //Name of the person with whom you want to start a new conversation
-    $scope.newConvoName = '';
-
-
     // Message Logic
     $scope.displayMessages = function() {
-
-    };
-    $scope.updateMessages = function() {
-
+      $scope.currentMessageList = $firebaseArray(myFirebaseRef.child('messages'));
     };
     $scope.sendMessage = function() {
-        $scope.currentMessageList.push($scope.username +': ' + $scope.enteredText);
-        Inbox.sendMessage({text: $scope.enteredText});
+        $scope.currentMessageList.$add({message : $scope.enteredText, to: $scope.currentRecipient, from: $scope.username});
         $scope.enteredText = '';
     };
 
     // Conversation Logic
-    $scope.displayConversations = function() {
-
+    $scope.getAllUsers = function(){
+      connectModel.getAllUsers().then(function(r) {
+          $scope.conversationList = r.data;
+      });
     };
-    $scope.updateConversations = function() {
 
-    };
     $scope.switchConversation = function(conversation) {
-        // console.log($scope.username);
-        $scope.currentMessageList = $scope.conversationObject[conversation];
         $scope.currentRecipient = conversation.username;
-
-        var arr = [$scope.currentRecipient, $scope.username];
-        arr.sort();
-
-        console.log(arr[0] + arr[1]);
+        var arr = [$scope.currentRecipient, $scope.username].sort();
         var convoURL = baseURL + arr[0] + arr[1];
-        var myFirebaseRef = new Firebase(convoURL);
-        // myFirebaseRef.set({'rachel': 'How goes it'});
-        var messages = $firebaseArray(myFirebaseRef.child('messages'));
-
-        messages.$add({count : 'Arr'});
-        console.log(messages);
-
-        console.log($firebaseArray(myFirebaseRef.child('messages')));
-
-        //We can generate new conversations
-
-        //Refactor above code and split it up
-        //Add scope variable online 72
-        //
-
-        // console.log(myFirebaseRef.child('messages').get());
-        // $scope.messages = $firebaseObject(myFirebaseRef.limit(15));
-        // $scope.messages({'sup': 'sup'});
+        myFirebaseRef = new Firebase(convoURL);
+        $scope.displayMessages();
     };
-    $scope.startNewConversation = function() {
-        $scope.conversationList.push($scope.newConvoName);
-        $scope.conversationObject[$scope.newConvoName] = [];
-        $scope.newConvoName = '';
 
-    };
-    $scope.deleteConversation = function() {
-
-    };
+    $scope.getAllUsers();
 
   }]);
