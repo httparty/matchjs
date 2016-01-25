79c8ae0b491c961e3a11340aaf3a80775955f5a8
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-25 11:05:52 -0800

    [refactor] Change pagination to use ngRepeat

--- client/app/components/connect/connectController.js
+++ client/app/components/connect/connectController.js
@@ -6,6 +6,7 @@
 
       var vm = this;
       vm.users = '';
+      vm.pages = [1, 2, 3, 4, 5];
 
       vm.getAllUsers = function() {
         connectModel.getAllUsers().then(function(r) {
@@ -14,7 +15,7 @@
       });
 
     };
-    
+
   }]);
 
 })();
