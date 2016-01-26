60bafbb693289f32f928c805259c8b4ce1be3673
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-26 11:49:26 -0800

    [feat] complete mvp of recommender to include generating karma rank and toTeach rank

--- server/recommender/recommend.js
+++ server/recommender/recommend.js
@@ -1,28 +1,80 @@
 var _ = require('underscore');
+var rank = {};
 
 exports.getRecommendations = function(usersArray, username) {
-  //get the object with the current user and take it out
-  //of the usersArray
-  console.log("HeY YOOOOOOO");
-  var current_user = {};
+
+  var currentUser = {};
+
   for (var i = 0; i < usersArray.length; i++) {
     if (usersArray[i].dataValues.username === username) {
-      current_user = usersArray[i].dataValues;
+      currentUser = usersArray[i].dataValues;
       usersArray.splice(i, 1);
     }
-  }
+  };
 
+  //populate rank object with appropriate ranks
+  generateToTeachRank(currentUser, usersArray);
+  generateKarmaRank(currentUser, usersArray);
+
+  //sort usersArray based on rank number in rank object
   usersArray.sort(function(a, b){
-    return b.dataValues.karmaPoints - a.dataValues.karmaPoints;
-  });
-  
-  usersArray.forEach(function(item){
-    console.log(item.dataValues.karmaPoints, item.dataValues.username);
+    return rank[b.dataValues.username] - rank[a.dataValues.username];
   });
 
-  // console.log(usersArray);
-  //we want to sort usersArray by comparing every elt to current_user and
-  //assigning some sort of ranking
+  //test to make sure rank sorting is happening correctly
+  _.each(usersArray, function(user) {
+    console.log("USER", user.dataValues.username, rank[user.dataValues.username]);
+  })
 
-  return usersArray;
+  //return top 6 recommended users
+  return usersArray.slice(0,6);
 };
+
+/*************************************************************
+Generates a value that represents the number of skills
+a user is willing to teach that the current user would like
+to learn
+**************************************************************/
+
+var generateToTeachRank = function(currentUser, usersArray) {
+
+  _.each(usersArray, function(user){
+    var teachRank = _.intersection(currentUser.toLearn, user.dataValues.toTeach).length;
+    rank[user.dataValues.username] = (rank[user.dataValues.username] || 0)  + teachRank;
+  });
+  // console.log("RANK", rank);
+};
+
+/*************************************************************
+Generates the total karma points for a user
+**************************************************************/
+
+var generateKarmaRank = function(currentUser, usersArray) {
+
+  _.each(usersArray, function(user){
+    var karmaRank = user.dataValues.karmaPoints;
+    rank[user.dataValues.username] = (rank[user.dataValues.username] || 0)  + karmaRank;
+  });
+  // console.log("RANK", rank);
+};
+
+/*************************************************************
+Generates a vluae that represents the number of skills
+a user would like to learn that the current user would
+also like to learn
+**************************************************************/
+
+var generateToLearnRank = function(currentUser, usersArray) {
+
+}
+
+/*************************************************************
+Write additional generate rank functions here!
+
+Remember to use this syntax:
+rank[user.dataValues.username] = (rank[user.dataValues.username] || 0)  + rank;
+
+This populates starting rank with 0 if no rank has been supplied yet.
+
+This will be necessary when we add in logic that skips over ranks.
+**************************************************************/
