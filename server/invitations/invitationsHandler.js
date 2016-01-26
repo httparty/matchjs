0b80784cd3dca8e9e33cbef7800feafebd2bc49c
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-26 15:23:03 -0800

    [feat] add get routes to get invitations by sender and recipient

--- server/invitations/invitationsHandler.js
+++ server/invitations/invitationsHandler.js
@@ -6,7 +6,25 @@ module.exports = {
     var invitee = req.body.invitee;
     var sessionInfo = req.body.sessionInfo;
 
-    helpers.createInvitation(username,invitee,sessionInfo);
-    res.send('Post successful');
+    helpers.createInvitation(username,invitee,sessionInfo)
+    .then(function(invitation) {
+      res.send(invitation);
+    });
+  },
+  getInvitationsBySender: function(req, res) {
+    var username = req.params.username;
+
+    helpers.getInvitationsBySender(username)
+    .then(function(invitations) {
+      res.send(invitations);
+    });
+  },
+  getInvitationsByRecipient: function(req, res) {
+    var username = req.params.username;
+
+    helpers.getInvitationsByRecipient(username)
+    .then(function(invitations) {
+      res.send(invitations);
+    });
   }
 };
