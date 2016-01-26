308f0fa48b0a20a9a5e73036aed26d185f8b725f
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-26 11:53:14 -0800

    [refactor] resolve syntax errors in recommender

--- server/recommender/recommend.js
+++ server/recommender/recommend.js
@@ -1,33 +1,17 @@
 var _ = require('underscore');
 var rank = {};
 
-exports.getRecommendations = function(usersArray, username) {
+/*************************************************************
+Generates the total karma points for a user
+**************************************************************/
 
-  var currentUser = {};
+var generateKarmaRank = function(currentUser, usersArray) {
 
-  for (var i = 0; i < usersArray.length; i++) {
-    if (usersArray[i].dataValues.username === username) {
-      currentUser = usersArray[i].dataValues;
-      usersArray.splice(i, 1);
-    }
-  };
-
-  //populate rank object with appropriate ranks
-  generateToTeachRank(currentUser, usersArray);
-  generateKarmaRank(currentUser, usersArray);
-
-  //sort usersArray based on rank number in rank object
-  usersArray.sort(function(a, b){
-    return rank[b.dataValues.username] - rank[a.dataValues.username];
+  _.each(usersArray, function(user){
+    var karmaRank = user.dataValues.karmaPoints;
+    rank[user.dataValues.username] = (rank[user.dataValues.username] || 0)  + karmaRank;
   });
-
-  //test to make sure rank sorting is happening correctly
-  _.each(usersArray, function(user) {
-    console.log("USER", user.dataValues.username, rank[user.dataValues.username]);
-  })
-
-  //return top 6 recommended users
-  return usersArray.slice(0,6);
+  // console.log("RANK", rank);
 };
 
 /*************************************************************
@@ -46,19 +30,6 @@ var generateToTeachRank = function(currentUser, usersArray) {
 };
 
 /*************************************************************
-Generates the total karma points for a user
-**************************************************************/
-
-var generateKarmaRank = function(currentUser, usersArray) {
-
-  _.each(usersArray, function(user){
-    var karmaRank = user.dataValues.karmaPoints;
-    rank[user.dataValues.username] = (rank[user.dataValues.username] || 0)  + karmaRank;
-  });
-  // console.log("RANK", rank);
-};
-
-/*************************************************************
 Generates a vluae that represents the number of skills
 a user would like to learn that the current user would
 also like to learn
@@ -66,7 +37,7 @@ also like to learn
 
 var generateToLearnRank = function(currentUser, usersArray) {
 
-}
+};
 
 /*************************************************************
 Write additional generate rank functions here!
@@ -78,3 +49,32 @@ This populates starting rank with 0 if no rank has been supplied yet.
 
 This will be necessary when we add in logic that skips over ranks.
 **************************************************************/
+
+exports.getRecommendations = function(usersArray, username) {
+
+  var currentUser = {};
+
+  for (var i = 0; i < usersArray.length; i++) {
+    if (usersArray[i].dataValues.username === username) {
+      currentUser = usersArray[i].dataValues;
+      usersArray.splice(i, 1);
+    }
+  }
+
+  //populate rank object with appropriate ranks
+  generateToTeachRank(currentUser, usersArray);
+  generateKarmaRank(currentUser, usersArray);
+
+  //sort usersArray based on rank number in rank object
+  usersArray.sort(function(a, b){
+    return rank[b.dataValues.username] - rank[a.dataValues.username];
+  });
+
+  //test to make sure rank sorting is happening correctly
+  // _.each(usersArray, function(user) {
+  //   console.log('USER', user.dataValues.username, rank[user.dataValues.username]);
+  // });
+
+  //return top 6 recommended users
+  return usersArray.slice(0,6);
+};
