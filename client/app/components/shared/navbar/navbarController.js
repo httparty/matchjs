d1fb03d2dcb9db4d9418334505dd105da5835df8
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-26 08:07:41 -0800

    [feat] add navbar model. update navbar to redirect to dynamic profile url

--- client/app/components/shared/navbar/navbarController.js
+++ client/app/components/shared/navbar/navbarController.js
@@ -2,13 +2,19 @@
   'use strict';
 
   angular.module('app.navbar', [])
-    .controller('NavController', ['AuthService', function (AuthService) {
+    .controller('NavController', ['AuthService', '$state', function (AuthService, $state) {
       var vm = this;
 
       vm.user = angular.fromJson(AuthService.getCurrentUser());
 
       vm.isLoggedIn = function() {
         return AuthService.isAuthenticated();
+      };
+
+      //DRY refactor needed: this function is copied over from connectController
+      vm.getThisUserProfile = function(username) {
+        console.log('in navbarController: user', username);
+        $state.go('profile', {username: username});
       };
 
     }]);
