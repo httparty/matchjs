f524f8061555a6708e0e84019e0ed4b41c5084e2
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-28 17:12:12 -0800

    [feat] add search server feature to handle client side query builder

---
+++ server/search/search.js
@@ -0,0 +1,49 @@
+var _ = require('underscore');
+
+exports.getSearchResults = function(queryParams, usersArray) {
+
+  // console.log(queryParams);
+
+  result = usersArray;
+
+  if (!_.isEmpty(queryParams)) {
+
+    if (queryParams.locations) {
+
+      if (typeof queryParams.locations === 'string') {
+        queryParams.locations = [queryParams.locations];
+      }
+
+      result = _.filter(result, function(item) {
+        return _.contains(queryParams.locations, item.dataValues.location);
+      });
+      // console.log("has some locations");
+
+    }
+
+    if (queryParams.skills) {
+      // console.log("only skills");
+
+      if (typeof queryParams.skills === 'string') {
+        queryParams.skills = [queryParams.skills];
+      }
+
+      result = _.filter(result, function(item) {
+        return _.intersection(queryParams.skills, item.dataValues.toTeach).length > 0
+      });
+
+      // _.each(result, function(item) {
+      //   console.log(item.dataValues.toTeach)
+      // });
+
+      // _.each(result, function(item) {
+      //   console.log("check", _.intersection(req.query.skills, item.dataValues.toTeach));
+      // })
+    }
+    return result;
+
+  } else {
+    return result;
+  }
+
+};
