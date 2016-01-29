abf8d76f173efe64b2ea36f681025f76db7c778f
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-29 13:57:48 -0800

    [refactor] change karma conf

--- client/test/unit/config/karma.conf.js
+++ client/test/unit/config/karma.conf.js
@@ -9,9 +9,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../bower_components/angular/angular.js',
-      './unit/*.js',
-      '../app/app.js'
+      '../../../../node_modules/angular/angular.js',
+      '../../../../node_modules/angular-mocks/angular-mocks.js',
+      './../*.js'
     ],
 
     // test result reporter
