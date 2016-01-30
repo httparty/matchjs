3cf0d44e8a2235604e8ae6d84cf619bf6c43832e
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-30 15:49:35 -0800

    [feat] Add client-side delete user functionality

--- client/app/components/settings/settingsController.js
+++ client/app/components/settings/settingsController.js
@@ -2,9 +2,11 @@
   'use strict';
   angular.module('app.settings', [])
   .controller('settingsController', [
-    'settingsModel', function(settingsModel){
+    'settingsModel', '$state', function(settingsModel, $state){
 
     var vm = this;
+    vm.username = $state.params;
+
 
     //Allows user to edit preferences page
     vm.editPreferences = function() {
@@ -14,6 +16,13 @@
     //Saves user preferences
     vm.savePreferences = function() {
 
+    };
+
+    vm.deleteAccount = function() {
+      console.log('Account Deleted');
+      if(confirm('Are you sure you want to delete your account?')){
+        settingsModel.deleteAccount({username: vm.username});
+      }
     };
 
   }]);
