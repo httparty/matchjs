064b204453c6125997a7a2ddf4964b666e377c02
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-30 15:49:35 -0800

    [feat] Add client-side delete user functionality

--- client/app/components/settings/settingsModel.js
+++ client/app/components/settings/settingsModel.js
@@ -5,20 +5,29 @@
     .factory('settingsModel', function($http){
       var model = {};
 
-      model.savePreferences = function(settingsObj){
+      model.savePreferences = function(userObj){
         return $http({
           method: 'POST',
-          url: '/api/settings/save' + settingsObj.username
+          url: '/api/settings/save/' + userObj.username
         });
 
       };
 
-      model.editPreferences = function(settingsObj){
+      model.editPreferences = function(userObj){
         return $http({
           method: 'GET',
-          url: '/api/settings/edit' + settingsObj.username
+          url: '/api/settings/edit/' + userObj.username
         });
       };
+
+      model.deleteAccount = function(userObj){
+        return $http({
+          method: 'GET',    //Change to delete eventually
+          url: '/api/settings/deleteAccount/' + userObj.username    //May need to add username property on top
+        });
+      };
+
+      return model;
 
   });
 })();
