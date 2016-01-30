649bbd5c9eacd88d450e0281982b47c680349ca6
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-30 12:07:30 -0800

    [feat] add addPadawan request function

--- client/app/components/profile/profileModel.js
+++ client/app/components/profile/profileModel.js
@@ -20,9 +20,27 @@ angular.module('app.profile')
     });
   };
 
+  var addPadawan = function(mentor, padawan) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/addPadawan/' + mentor.username,
+      data: padawan
+    });
+  };
+
+  var deletePadawan = function(mentor, padawan) {
+    // return $http({
+    //   method: 'POST',
+    //   url: '/api/users/profile' + mentor,
+    //   data: padawan
+    // })
+  };
+
   return {
     getUserProfile : getUserProfile,
-    updateProfile : updateProfile
+    updateProfile : updateProfile,
+    addPadawan : addPadawan,
+    deletePadawan : deletePadawan
   };
 
 }]); 
