00dafd3bc34654572fe102126b8046c2f1b4c509
Sergey Piterman <sergeypiterman@gmail.com>
2016-01-30 15:43:02 -0800

    [feat] Add invitation email notification first pass

--- server/invitations/invitationsHandler.js
+++ server/invitations/invitationsHandler.js
@@ -1,21 +1,28 @@
 var helpers = require('../db/helpers.js');
 
+var emailer = require('../email/emailHandler.js')
+
 module.exports = {
   createInvitation: function(req,res) {
-    var username = req.cookies['user-profile'].username;
+    // var username = req.cookies['user-profile'].username;
+    var username = req.body.mentor;
+
     var invitee = req.body.mentee;
     var sessionInfo = req.body.sessionInfo;
-    console.log('Invitee', invitee);
-    console.log('Username', username);
-    console.log('SessionInfo', sessionInfo);
+    // console.log('Invitee', invitee);
+    // console.log('Username', username);
+    // console.log('SessionInfo', sessionInfo);
+    console.log(username, "HEY THIS IS THE USERNAME PULLED FROM THE COOKIES!!!");
+    console.log(req.body, "This is the invitation Object on the server");
 
+    emailer.invitationConfirm(req, res);
 
     helpers.createInvitation(username,invitee,sessionInfo)
     .then(function(invitation) {
       res.send(invitation);
     });
   },
-  
+
   getInvitationsBySender: function(req, res) {
     var username = req.params.username;
 
