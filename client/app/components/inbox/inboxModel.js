05650461552143b8d8cde26847fe90937834232f
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-30 16:02:09 -0800

    [refactor] inbox component service

--- client/app/components/inbox/inboxModel.js
+++ client/app/components/inbox/inboxModel.js
@@ -2,44 +2,44 @@
   'use strict';
 
   angular.module('app.inbox')
-    .factory('inboxModel', ['$http', function($http) {
-
+    .factory('inboxModel', [ function() {
       var inbox = {};
 
-      //get all currently open chat threads for a user
-      inbox.getAllFirebaseConvos = function(username, firebaseUrl, cb) {
+      inbox.getAllFirebaseConvos = function(username, firebaseUrl, callback) {
+
         var firebaseConnection = new Firebase(firebaseUrl);
+
+          //list of all users the current user has had
+          //a conversation with
+          var userConversationHistory = {};
+
+          //a mapping between conversation name
+          //and time last updated
+          var comparator = {};
 
           firebaseConnection.on("value", function(snapshot) {
 
+            //get all Firebase user conversations 
             var allFirebaseConvos = snapshot.val();
-            //iterate through convos
-            //filter by ones that have your name in them
-            var result = {};
 
-            result = _.map(allFirebaseConvos, function(item, index) {
-              // return index;
+            //Format data from Firebase
+            userConversationHistory = _.map(allFirebaseConvos, function(item, index) {
               return {conversation: index, updated: item.updated};
             });
-            
-            //remove substring username
-            var test = {};
 
-            result = _.map(result, function(item) {
-
+            //Remove current user name from conversation name
+            //in order to properly format
+            //Get timestamps for comparator mapping
+            userConversationHistory = _.map(userConversationHistory, function(item) {
               var updatedItem = item.conversation.replace(username, "");
-              console.log("item", updatedItem);
-              // return {name: updatedItem, updated: item.updated};
-              test[updatedItem] = item.updated;
-
+              comparator[updatedItem] = item.updated;
               return updatedItem;
-
             });
 
-            var mapping = [test, result];
+            var mapping = [comparator, userConversationHistory];
 
-            if (cb) {
-              cb(mapping);
+            if (callback) {
+              callback(mapping);
             }            
           }, function (errorObject) {
             console.log("Cannot connect to Firebase: " + errorObject.code);
