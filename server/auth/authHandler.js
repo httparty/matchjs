28e3f508d18edb5d4f0e7e548cc29b1f99fe2b84
Polina Soshnin <psoshnin@wellesley.edu>
2016-02-01 16:16:24 -0800

    [refactor] redirect new users to /#/auth who have never logged in and do not have a public email in their github profile

--- server/auth/authHandler.js
+++ server/auth/authHandler.js
@@ -1,48 +1,57 @@
 var passport = require('passport');
 var helpers = require('../db/helpers.js');
 
+var setCookieProfile = function(userObj) {
+  var profile = {
+    id: userObj.id,
+    username: userObj.username,
+    displayName: userObj.displayName,
+    email: userObj._json.email,
+    avatar: userObj._json.avatar_url,
+    location: userObj._json.location,
+    github : userObj.profileUrl
+  }
+  return profile;
+};
+
 module.exports = {
   initialLogin: passport.authenticate('github'),
+
   redirect:  passport.authenticate('github', {failureRedirect: '/failure'}),
-  // if user doesn't exist, create
-  // if user exists, save them in the db
 
   success: function(req, res) {
   
-  console.log("USER PROFILE", req.user);
+    helpers.signInUser(req.user)
+    .then(function(user) {
 
-  // return user if found
-  //in database
-  helpers.signupUser(req.user)
-  .then(function(user) {
+      //if user found, set cookie information
+      if (user) {
+        var cookie = setCookieProfile(req.user);
+        res.cookie('user-profile', cookie, { maxAge: 2592000000 });  // Expires in one month
+        res.redirect('/api/email/signupConfirm');
+      } else {
 
-    //if user found, set cookie information
-    if (user) {
+        //if user not found and email is available
+        if (req.user._json.email) {
+          //add user to DB and set cookie profile
+          helpers.addUser(req.user)
+          .then(function(user) {
 
-    //set cookie information
-    var profile = {
-      id: req.user.id,
-      username: req.user.username,
-      displayName: req.user.displayName,
-      email: req.user._json.email,
-      avatar: req.user._json.avatar_url,
-      location: req.user._json.location,
-      github : req.user.profileUrl
-    };
-    res.cookie('user-profile', profile, { maxAge: 2592000000 });  // Expires in one month
-    // Successful authentication, redirect home.
-    // res.redirect('/#/connect');
-    res.redirect('/api/email/signupConfirm');
+            var cookie = setCookieProfile(req.user);
+            res.cookie('user-profile', cookie, { maxAge: 2592000000 });  // Expires in one month
+            res.redirect('/api/email/signupConfirm');
+          });
+
+        } else {
+
+          console.log("USER NOT FOUND IN DB WITH NO EMAIL");
+          //if user not found and email is not available
+          res.redirect('/#/auth');
+          //send to auth/getemail route
+
+          
+        }
+      }
     });
-      
-    } else {
-      //if user not found and email is available
-
-
-      //if user not found and email is not available
-      //send to auth/getemail route
-
-      
-    }
   }
 };
