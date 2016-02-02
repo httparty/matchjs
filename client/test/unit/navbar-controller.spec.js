9ca13444b241cc416b6fcaae0f76d3b87b651d0d
Polina Soshnin <psoshnin@wellesley.edu>
2016-02-02 09:40:10 -0800

    [feat] add navbar controller spec

---
+++ client/test/unit/navbar-controller.spec.js
@@ -0,0 +1,34 @@
+describe('Controller: NavController', function() {
+
+  var AuthService;
+  var $state;
+
+  beforeEach(module('app'));
+  beforeEach(inject(function ($injector) {
+
+    // $rootScope = $injector.get('$rootScope');
+    // $scope = $rootScope.$new();
+    // $httpBackend = $injector.get('$httpBackend');
+    $controller = $injector.get('$controller');
+    AuthService = $injector.get('AuthService');
+    $state = $injector.get('$state');
+
+    //Need to mock a cookie
+    // NavController = $controller('NavController', {
+    //   $state: $state,
+    //   AuthService : AuthService
+    // });
+
+  }));
+
+  afterEach(function () {
+    //any after each goes here
+  });
+
+  describe('vm.email', function () {
+
+   it('should equal 4', function() {
+      expect("4").toBe("4");
+    });
+  });
+});
