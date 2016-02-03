e50aabe0ef15c19af9eb20a3195dd5d1638668db
Rachel Jenkins <jenkinsr07@gmail.com>
2016-02-02 18:26:48 -0800

    [dev] profile controller test outline

---
+++ client/test/unit/profile-controller.spec.js
@@ -0,0 +1,31 @@
+describe('Controller: ProfileController', function() {
+
+  var $httpsBackend;
+  var AuthService;
+  var $rootScope;
+  var $window;
+  var $state;
+  var Profile;
+  var $sce;
+  var invitationsModel;
+  var $location;
+
+  beforeEach(module('app'));
+  beforeEach(inject(function($injector) {
+
+  }));
+
+  afterEach(function() {
+
+  });
+
+  describe('', function() {
+    it('should...', function() {
+
+    });
+    it('should equal...', function() {
+      expect('x').toBe('x');
+    });
+  })
+
+})
