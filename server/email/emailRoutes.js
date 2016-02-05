277f4ee383497a3b6b8c5a6b0fc0ff83aa9456a8
Polina Soshnin <psoshnin@wellesley.edu>
2016-02-04 17:17:54 -0800

    [refactor] remove unused route

--- server/email/emailRoutes.js
+++ server/email/emailRoutes.js
@@ -1,6 +1,5 @@
 var emailHandler = require('./emailHandler.js');
 
 module.exports = function(app) {
-  app.get('/signupConfirm',emailHandler.signupConfirm);
   app.post('/sentMessage/',emailHandler.sentMessage);
 };
