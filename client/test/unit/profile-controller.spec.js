b3342ddb8ff53add3960edd92c560ffdadb24d81
Sergey Piterman <sergeypiterman@gmail.com>
2016-02-04 19:38:41 -0800

    [feat] Add several client-side tests

--- client/test/unit/profile-controller.spec.js
+++ client/test/unit/profile-controller.spec.js
@@ -12,6 +12,14 @@ describe('Controller: ProfileController', function() {
 
   beforeEach(module('app'));
   beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $scope = $rootScope.$new();
+    $httpBackend = $injector.get('$httpBackend');
+    $controller = $injector.get('$controller');
+    AuthService = $injector.get('AuthService');
+    invitationsModel = $injector.get('invitationsModel');
+    $state = $injector.get('$state');
+    Profile = $injector.get('Profile');
 
   }));
 
@@ -19,13 +27,42 @@ describe('Controller: ProfileController', function() {
 
   });
 
-  describe('', function() {
-    it('should...', function() {
+  describe('Profile Model Testing', function(){
 
-    });
-    it('should equal...', function() {
-      expect('x').toBe('x');
-    });
-  })
+    describe('Basic Tests', function(){
 
-})
+      it('should be an object', function(){
+        expect(typeof Profile).toBe('object');
+      });
+
+      it('should have a getUserProfile method', function() {
+        expect(typeof Profile.getUserProfile).toBe('function');
+      });
+
+      it('should have an updateProfile method', function(){
+        expect(typeof Profile.updateProfile).toBe('function');
+      });
+
+      it('should have an addPadawan method', function(){
+        expect(typeof Profile.addPadawan).toBe('function');
+      });
+      //Make sure it has all the other methods
+
+    });//End Basic Tests
+
+    });//End Profile Model Testing
+
+
+
+  // describe('', function() {
+  //   it('should...', function() {
+
+  //   });
+
+  //   it('should equal...', function() {
+  //     expect('x').toBe('x');
+  //   });
+
+  // });
+
+});
