32b21c2df8e0f83666410eda62a9baa3adf1a35f
Polina Soshnin <psoshnin@wellesley.edu>
2016-02-05 19:52:00 -0800

    [test] complete connect controller spec

--- client/test/unit/connect-controller.spec.js
+++ client/test/unit/connect-controller.spec.js
@@ -10,17 +10,21 @@ describe('Controller: ConnectController', function() {
 
   beforeEach(module('app'));
   beforeEach(module('mock.auth-service')); 
-  beforeEach(inject(function ($injector, _MockAuthService_) {
+  beforeEach(module('mock.connect-service')); 
+  beforeEach(inject(function ($q, $injector, _MockAuthService_, _MockConnectService_) {
+    deferred = $q.defer();
 
     $rootScope = $injector.get('$rootScope');
     $scope = $rootScope.$new();
-    $httpBackend = $injector.get('$httpBackend');
     $controller = $injector.get('$controller');
     AuthService = _MockAuthService_;
-    connectModel = $injector.get('connectModel')
+    connectModel = _MockConnectService_;
     $state = $injector.get('$state');
 
-    spyOn($rootScope, '$broadcast');
+    spyOn($rootScope, '$broadcast').and.callThrough();
+    spyOn(connectModel, 'getAllUsersRec').and.returnValue(deferred.promise);
+    spyOn(connectModel, 'getAllUsers').and.returnValue(deferred.promise);
+    spyOn(connectModel, 'searchUsers').and.returnValue(deferred.promise);
     
     ConnectController = $controller('connectController', {
       $scope: $scope,
@@ -33,37 +37,37 @@ describe('Controller: ConnectController', function() {
 
   describe('Controller Variables', function () {
 
-    it('it should contain a users property', function() {
+    it('Should contain a users property', function() {
       expect(ConnectController.users).toBeDefined();
       expect(angular.isString(ConnectController.users)).toBe(true);
     });
 
-    it('it should contain an allCities object', function() {
+    it('Should contain an allCities object', function() {
       expect(ConnectController.allCities).toBeDefined();
       expect(angular.isObject(ConnectController.allCities)).toBe(true);
     });
 
-    it('it should contain an allNames object', function() {
+    it('Should contain an allNames object', function() {
       expect(ConnectController.allNames).toBeDefined();
       expect(angular.isObject(ConnectController.allNames)).toBe(true);
     });
 
-    it('it should contain a locations array', function() {
+    it('Should contain a locations array', function() {
       expect(ConnectController.locations).toBeDefined();
       expect(angular.isArray(ConnectController.locations)).toBe(true);
     });
 
-    it('it should contain a names array', function() {
+    it('Should contain a names array', function() {
       expect(ConnectController.names).toBeDefined();
       expect(angular.isArray(ConnectController.names)).toBe(true);
     });
 
-    it('it should contain a skills array', function() {
+    it('Should contain a skills array', function() {
       expect(ConnectController.skills).toBeDefined();
       expect(angular.isArray(ConnectController.skills)).toBe(true);
     });
 
-    it('it should contain a tags array', function() {
+    it('Should contain a tags array', function() {
       expect(ConnectController.tags).toBeDefined();
       expect(angular.isArray(ConnectController.tags)).toBe(true);
     });
@@ -140,11 +144,19 @@ describe('Controller: ConnectController', function() {
       ConnectController.addLocation("San Francisco");
       expect(ConnectController.tags.length).toBe(1);
     });
+
+    it("Adding multiple distinct locations adds all of them to tags array", function() {
+      expect(ConnectController.tags.length).toBe(0);
+      ConnectController.addLocation("San Francisco");
+      expect(ConnectController.tags.length).toBe(1);
+      ConnectController.addLocation("New York");
+      expect(ConnectController.tags.length).toBe(2);
+    });
   });
 
   describe('Adding skills to search query', function () {
 
-    it("Adding a new skills adds a query param to tags array", function() {
+    it("Adding a new skill adds a query param to tags array", function() {
       expect(ConnectController.tags.length).toBe(0);
       ConnectController.addSkill("AngularJS");
       expect(ConnectController.tags.length).toBe(1);
@@ -157,11 +169,19 @@ describe('Controller: ConnectController', function() {
       ConnectController.addSkill("AngularJS");
       expect(ConnectController.tags.length).toBe(1);
     });
+
+    it("Adding multiple distinct skills adds all of them to tags array", function() {
+      expect(ConnectController.tags.length).toBe(0);
+      ConnectController.addSkill("AngularJS");
+      expect(ConnectController.tags.length).toBe(1);
+      ConnectController.addSkill("React");
+      expect(ConnectController.tags.length).toBe(2);
+    });
   });
 
   describe('Adding names to search query', function () {
 
-    it("Adding a new names adds a query param to tags array", function() {
+    it("Adding a new name adds a query param to tags array", function() {
       expect(ConnectController.tags.length).toBe(0);
       ConnectController.addName("Vivario");
       expect(ConnectController.tags.length).toBe(1);
@@ -173,6 +193,56 @@ describe('Controller: ConnectController', function() {
       expect(ConnectController.tags.length).toBe(1);
       ConnectController.addName("Vivario");
       expect(ConnectController.tags.length).toBe(1);
+    });
+
+    it("Adding multiple distinct names adds all of them to tags array", function() {
+      expect(ConnectController.tags.length).toBe(0);
+      ConnectController.addName("Vivario");
+      expect(ConnectController.tags.length).toBe(1);
+      ConnectController.addName("Dotio");
+      expect(ConnectController.tags.length).toBe(2);
+    });
+  });
+
+  describe('Adding names, skills, and locations to search query', function () {
+    it("Adding multiple distinct names, skills, and locations adds all of them to tags array", function() {
+      expect(ConnectController.tags.length).toBe(0);
+      ConnectController.addName("Vivario");
+      expect(ConnectController.tags.length).toBe(1);
+      ConnectController.addName("Dotio");
+      expect(ConnectController.tags.length).toBe(2);
+      ConnectController.addName("Dotio");
+      expect(ConnectController.tags.length).toBe(2);
+      ConnectController.addSkill("AngularJS");
+      expect(ConnectController.tags.length).toBe(3);
+      ConnectController.addLocation("New York");
+      ConnectController.addLocation("New York");
+      expect(ConnectController.tags.length).toBe(4);
+    });
+  });
+
+  describe('Asynchronous calls to connect service', function() {
+
+    it('should call getAllUsersRec on connect service', function() {
+      ConnectController.getAllUsersRec();
+      expect(connectModel.getAllUsersRec).toHaveBeenCalled();
+      expect(connectModel.getAllUsersRec.calls.count()).toBe(1);
+    });
+
+    it('should call getAllUsers on connect service', function() {
+      ConnectController.getAllUsers();
+      expect(connectModel.getAllUsers).toHaveBeenCalled();
+      expect(connectModel.getAllUsers.calls.count()).toBe(1);
+    });
+  });
+
+  describe('Listening to Event Broadcasts', function () {
+
+    it('broadcasting query-changed triggers call to searchUsers', function() {
+      $rootScope.$broadcast('query-changed');
+      expect($rootScope.$broadcast).toHaveBeenCalledWith('query-changed');
+      expect(connectModel.searchUsers).toHaveBeenCalled();
+      expect(connectModel.searchUsers.calls.count()).toBe(1);
     });
   });
 });
