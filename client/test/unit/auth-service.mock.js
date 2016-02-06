04605ec4157c7796646a11560122a04870e499c1
Polina Soshnin <psoshnin@wellesley.edu>
2016-02-05 19:17:07 -0800

    [test] rename auth service mock and add connect service mock

---
+++ client/test/unit/auth-service.mock.js
@@ -0,0 +1,17 @@
+angular.module('mock.auth-service', []).
+  factory('MockAuthService', function() {
+    var MockAuthService = {};
+
+    MockAuthService.isAuthenticated = function() {
+      return true;
+    };
+    
+    MockAuthService.getCurrentUser = function() {
+      var mockCookieObj = { username: 'vivario'};
+      return JSON.stringify(mockCookieObj);
+    };
+    
+    return MockAuthService;
+});
+
+  
---
+++ client/test/unit/connect-service.mock.js
@@ -0,0 +1,22 @@
+angular.module('mock.connect-service', []).
+  factory('MockConnectService', function() {
+    var MockConnectService = {};
+
+    MockConnectService.getAllUsersRec = function() {
+      return true;
+    };
+    
+    MockConnectService.getAllUsers = function() {
+      return true;
+    };
+
+    MockConnectService.getThisUserProfile = function() {
+      return true;
+    };
+
+    MockConnectService.searchUsers = function() {
+      return true;
+    };
+    
+    return MockConnectService;
+});
